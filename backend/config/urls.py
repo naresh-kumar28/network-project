785761c345e1fd68b0ba2e55d7bebd57f0f1a1cd
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-08-11 10:57:44 +0530

    feat: initialize Add Swager in API configuration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API Docs",
+      default_version='v1',
+      description="Authentication",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/v1/', include('apps.accounts.urls')),
@@ -14,4 +33,5 @@ urlpatterns = [
     path('api/sales/v1/', include('apps.sales.urls')),
     path('api/kyc/v1/', include('apps.kyc.urls')),
     path('api/notifications/v1/', include('apps.notifications.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
